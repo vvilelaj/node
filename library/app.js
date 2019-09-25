79ea14e8877aa84ec3464ecef7b46792cb3052d1
Victor Vilela <vilela_victor@hotmail.com>
2019-09-25 18:01:36 -0500

    library : add ESLint package and fix mistakes reported by ESLint

--- library/app.js
+++ library/app.js
@@ -1,18 +1,18 @@
-var express = require("express");
-var path = require("path");
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, '/public/')));
 
-app.use("/css", express.static(path.join(__dirname,"/node_modules/","bootstrap/dist/css")));
-app.use("/js", express.static(path.join(__dirname,"/node_modules/","bootstrap/dist/js")));
-app.use("/js", express.static(path.join(__dirname,"/node_modules/","jquery/dist/")));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/', 'bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/', 'bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/', 'jquery/dist/')));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"/views/index.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
 app.listen(3000, () => {
-    console.log(`Listening on port 3000`);
+    console.log('Listening on port 3000');
 });
