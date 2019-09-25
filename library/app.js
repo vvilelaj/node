9bdab135a37ca7c0f49fb4ce59004d6c7eff63e4
Victor Vilela <vilela_victor@hotmail.com>
2019-09-25 16:41:33 -0500

    library : servin static content

--- library/app.js
+++ library/app.js
@@ -3,6 +3,8 @@ var path = require("path");
 
 var app = express();
 
+app.use(express.static(path.join(__dirname,"/public/")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
