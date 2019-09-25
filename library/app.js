aed2967c9be3e8ffab40fe66b435f8bacab2b74f
Victor Vilela <vilela_victor@hotmail.com>
2019-09-25 16:53:00 -0500

    library :  redirec route to physical files

--- library/app.js
+++ library/app.js
@@ -5,6 +5,10 @@ var app = express();
 
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.use("/css", express.static(path.join(__dirname,"/node_modules/","bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname,"/node_modules/","bootstrap/dist/js")));
+app.use("/js", express.static(path.join(__dirname,"/node_modules/","jquery/dist/")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
