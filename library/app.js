e052d107a9a1f49a0da3f38f196ddce84dee0d69
Victor Vilela <vilela_victor@hotmail.com>
2019-09-27 18:21:24 -0500

    library : configuring ESLint with prettier and airbnb code format

--- library/app.js
+++ library/app.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.use(
-	'/css',
-	express.static(path.join(__dirname, '/node_modules/', 'bootstrap/dist/css'))
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/", "bootstrap/dist/css"))
 );
 app.use(
-	'/js',
-	express.static(path.join(__dirname, '/node_modules/', 'bootstrap/dist/js'))
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/", "bootstrap/dist/js"))
 );
 app.use(
-	'/js',
-	express.static(path.join(__dirname, '/node_modules/', 'jquery/dist/'))
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/", "jquery/dist/"))
 );
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/views/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.listen(3000, () => {
-	// eslint-disable-next-line no-console
-	console.log('Listening on port 3000');
+  // eslint-disable-next-line no-console
+  console.log("Listening on port 3000");
 });
