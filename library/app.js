ca61563717e3365130ba8201d9c9e4a5af07984e
Victor Vilela <vilela_victor@hotmail.com>
2019-09-27 20:03:54 -0500

    library add : a index view with ejb

--- library/app.js
+++ library/app.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/public/")));
+// It sets up a public folder
+app.use("/public", express.static(__dirname + "/public"));
 
+// It creates virtual folders aiming to files inside folder' packages
 app.use(
   "/css",
   express.static(path.join(__dirname, "/node_modules/", "bootstrap/dist/css"))
@@ -18,10 +20,16 @@ app.use(
   express.static(path.join(__dirname, "/node_modules/", "jquery/dist/"))
 );
 
+// It sets up the views folder and the view engine
+app.set("views", "./views");
+app.set("view engine", "EJS");
+
+// It serves the index file
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  res.render("index");
 });
 
+// It sets up the port for the web app
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log("Listening on port 3000");
